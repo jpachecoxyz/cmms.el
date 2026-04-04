48c851445f8b34696f51b6d8824ed1bf828571fd
Javier Pacheco <jpacheco@cock.li>
2026-04-04 18:47:42 -0500

    add main files

---
+++ emms.el
@@ -0,0 +1,16 @@
+;;; emms.el --- Emacs Maintenance Management System -*- lexical-binding: t; -*-
+
+;; Author: Ing. Javier Pacheco
+;; Version: 0.1
+;; Package-Requires: ((emacs "29.1") (org "9.6"))
+;; Keywords: org, maintenance, assets
+;; URL: https://github.com/jpachecoxyz/emms.el
+
+;;; Commentary:
+
+;; org-asset provides a simple CMMS-style asset management
+;; system built on top of Org Mode.
+
+;;; Code:
+
+;;; emms.el ends here
